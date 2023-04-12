36e1a3846e1f1ac7b76e7f0d896f75d5bfc6867c
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-04-12 19:27:47 +0900

    [add]provide supabase cli

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
 import { router } from '@/routes/router'
+import { AuthContextProvider } from '@/hooks/useAuth'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
   </React.StrictMode>,
 )
