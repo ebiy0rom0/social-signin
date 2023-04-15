a4a52959ac9941683dd091b84180d9673e788aaf
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-04-16 04:05:22 +0900

    [upd]redirect

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,28 +5,31 @@ import { Session } from '@supabase/supabase-js'
 const Home: React.FC = () => {
   const [ session, setSession ] = useState<Session | null>(null)
   const [ id, setID ] = useState("")
-  const { supabase } = useAuth()
+  const { supabase, redirect } = useAuth()
 
   useEffect(() => {
     (async () => {
       const { data, error } = await supabase.auth.getSession()
-      if (error || data.session == null) {
-        alert('no session')
-        window.location.href = '/'
-      }
-      console.log(data)
+      if (error) redirect(error.message)
+      if (data.session === null) redirect('session failed')
+
+      // 確認用
+      setSession(data.session)
 
       const r = await fetch("http://localhost:8080/verify", {
         headers: {
           Authorization: "Bearer " + data.session?.access_token
         }
-      }).then(res => res.text()).catch(e => new Error(e))
+      }).then(res => res).catch(e => new Error(e))
 
-      if (typeof r === 'string') {
-        setID(r)
-      } else {
-        console.log(r)
+      if (r instanceof Error) {
+        console.log("ERROR:" + r.message)
+        return
       }
+      if (r.status === 401) redirect('unverified')
+
+      // サーバーからテキストでIDが返ってくる
+      setID(await r.text())
     })()
 
     // サンプルのため、画面離脱時は強制サインアウト
@@ -36,7 +39,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <p>my status:: { JSON.stringify(session) }</p>
-      <p>welcome { id }!</p>
+      <p>welcome! ID:{ id }!</p>
     </div> )
 }
 
